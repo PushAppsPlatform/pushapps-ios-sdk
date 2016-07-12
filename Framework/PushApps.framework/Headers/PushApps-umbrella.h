098d9d1b468d6862369aa32cef5f082ef8a7e516
Asaf Ron <asaf@groboot.com>
2016-07-12 10:13:13 +0200

    Updated to version 1.0.2

--- Framework/PushApps.framework/Headers/PushApps-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "PACommon.h"
-#import "PAErrorCodes.h"
-#import "PushApps.h"
-
-FOUNDATION_EXPORT double PushAppsVersionNumber;
-FOUNDATION_EXPORT const unsigned char PushAppsVersionString[];
-
--- Framework/PushApps.framework/Headers/PushApps.h
+++ Framework/PushApps.framework/Headers/PushApps.h
@@ -1,46 +1,19 @@
 //
-//  pushapps.h
-//  pushapps
+//  PushApps.h
+//  PushApps
 //
-//  Created by Grisha on 2/15/16.
-//  Copyright © 2016 pushapps.mobi. All rights reserved.
+//  Created by Asaf Ron on 04/07/2016.
+//  Copyright © 2016 PushApps. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-#import "PACommon.h"
 
-//! Project version number for pushapps.
-FOUNDATION_EXPORT double pushappsVersionNumber;
+//! Project version number for PushApps.
+FOUNDATION_EXPORT double PushAppsVersionNumber;
 
-//! Project version string for pushapps.
-FOUNDATION_EXPORT const unsigned char pushappsVersionString[];
+//! Project version string for PushApps.
+FOUNDATION_EXPORT const unsigned char PushAppsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <pushapps/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <PushApps/PublicHeader.h>
 
-typedef void (^PACompletionBlock)(id object, NSError *error);
 
-@interface PushApps : NSObject
-
-// check this flag to see if the Pushapps service is currently available
-+(BOOL)isPushAppsServiceAvailable;
-
-+(void)setDelegate:(id<PAPopupDelegate>)newDelegate;
-
-// initialize push notifications for the application
-+(void)registerForPushNotifications:(NSDictionary *)launchOptions;
-
-// register to the Pushapps service
-+(void)registerDeviceWithSdkKey:(NSString*)sdkKey;
-
-// send APNS device token to the Pushapps service
-+(void)setDevicePushToken:(NSData *)deviceToken;
-
-// processes push notification
-+(void)handleNotification:(NSDictionary *)userInfo;
-
-+(void)getWidgetFeedForParams:(NSDictionary *)params withCompletionBlock:(PACompletionBlock)completion;
-
-+(void)handleOpenUrl:(NSURL *)url;
-
-@end
